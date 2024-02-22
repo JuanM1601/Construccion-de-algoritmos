a080213f3d8e6f34f69a556f0ffd9a759adba2d7
JuanM1601 <you@example.com>
2024-02-22 12:34:30 -0500

    Calculo de Salario

--- Empleado/Empleado.py
+++ Empleado/Empleado.py
@@ -1,10 +1,46 @@
 class Empleado:
-#Aqui va el codigo de empleado
- """
- #Atributos"""
-nombre =''
-apellido =''
-"""
-# 1= Masculino y 2 = Femenino"""
-sexo =0
-salario =0
+    """---------------------------
+    #Atributos
+    ---------------------------"""
+    nombre=0
+    apellido=0
+
+    """----------------------------
+    #1 Masculino y 2 Femenino
+    ----------------------------"""
+    sexo=0
+    salario=0
+
+    """-----------------------------
+    #Metodos
+    -----------------------------"""
+
+    def CambiarSalario(self, nuevoSalario):
+        #Aqui va el codigo del metodo
+        return 0 
+    
+    def CambiarEmpleado(self, nNombre, nApellido, nSexo, nSalario):
+        #Aqui va el codigo del metodo
+        return None
+    
+    def ConsultarSalario(self):
+        #Aqui va el codigo
+        return self.salario    
+    
+    def ConsultarNombre(self):
+        #Aqui va el codigo
+        return self.nombre   
+    
+    def CosultarApellido(self):
+        #Aqui va el codigo
+        return self.apellido
+    
+    def CosultarNombreCompleto(self):
+        #Aqui va el codigo
+        return self.nombre +" "+ self.apellido   
+    
+    def AumentoSalarial(self):
+        nSalario = self.salario * 0.05  
+        nSalario = nSalario + self.salario
+        self.salario = nSalario
+        return "El nuevo salario es de: "+self.salario
