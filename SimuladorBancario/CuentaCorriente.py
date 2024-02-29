92c99a950ab109f76141807cb0121b8fdae2141f
JuanM1601 <160885176+JuanM1601@users.noreply.github.com>
2024-02-28 20:29:08 -0500

    Update CuentaCorriente.py

--- SimuladorBancario/CuentaCorriente.py
+++ SimuladorBancario/CuentaCorriente.py
@@ -1,4 +1,34 @@
+from CuentaAhorros import CuentaAhorros
+
 class CuentaCorriente:
-    """
-    Atributos"""
-    saldo =0
+
+    """-----------------------------
+    Atributos
+    -----------------------------"""
+    saldo = ""
+
+    """-----------------------------
+    Asociaciones
+    -----------------------------"""
+
+    saldoAhorros = CuentaAhorros()
+
+    """-----------------------------
+    Metodos
+    -----------------------------"""
+    def ConsultarSaldo(self):
+        #Aqui va el codigo
+        return self.saldo
+    
+    def ConsignarValor(self, saldo):
+        nSaldo = self.saldo + ""
+        self.saldo = nSaldo
+        return "Ingrese valor a consignar" + ""
+    
+    def RetirarValor(self, saldo):
+        nSaldo = self.saldo - ""
+        self.saldo = nSaldo
+        return "Ingrese valor a retirar" + ""
+    
+    def ConsultarSaldoCorriente(self):
+        return self.saldo
