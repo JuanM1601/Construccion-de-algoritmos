135768b3d004dc626754569fb19f0e0a9c009721
JuanM1601 <160885176+JuanM1601@users.noreply.github.com>
2024-02-28 20:27:35 -0500

    Empleado.py

--- Empleado/Empleado.py
+++ Empleado/Empleado.py
@@ -1,16 +1,22 @@
+from Fecha import Fecha
+
 class Empleado:
     """---------------------------
     #Atributos
     ---------------------------"""
-    nombre=0
-    apellido=0
+    nombre = 0
+    apellido = 0
 
     """----------------------------
     #1 Masculino y 2 Femenino
     ----------------------------"""
-    sexo=0
-    salario=0
-
+    sexo = 0
+    salario = 0
+    """-----------------------------
+    Asociaciones
+    -----------------------------"""
+    fechaNacimiento = Fecha
+    fechaIngreso = Fecha
     """-----------------------------
     #Metodos
     -----------------------------"""
@@ -44,3 +50,25 @@ class Empleado:
         nSalario = nSalario + self.salario
         self.salario = nSalario
         return "El nuevo salario es de: "+self.salario
+    
+    def DuplicarSalario(self):
+        #Aqui va el codigo
+        #Forma 1
+        #self.salario = self.salario*2 
+        self.salario *= 2
+
+    def CalcularSalarioAnual(self):
+        #Aqui va el codigo
+        #Forma 1 
+        SalarioAnual = self.salario*12
+        return SalarioAnual
+        #Forma 2
+        #return self.salario*12
+    
+    def ConsultarDiaCumpleaños(self):
+        return "El diaa de su cumpleaños es "+self.fechaNacimiento.ConsultarDia()
+    
+    def CalcularImpuesto(self):
+        #Forma 1
+        total = self.CalcularSalarioAnual()
+        return (total * 19.5) / 100
