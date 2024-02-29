e8624af69e25fa34d74e5b0ceee754037cc4b8fc
JuanM1601 <160885176+JuanM1601@users.noreply.github.com>
2024-02-28 20:28:49 -0500

    Update CuentaAhorros.py

--- SimuladorBancario/CuentaAhorros.py
+++ SimuladorBancario/CuentaAhorros.py
@@ -1,6 +1,42 @@
+from CuentaCorriente import CuentaCorriente
+
 class CuentaAhorros:
-    """
-    Atributos"""
+
+    """-----------------------------
+    Atributos
+    -----------------------------"""
     saldo =0
-    interesMensual =0
+
+    """-----------------------------
+    Asociaciones
+    -----------------------------"""
+
+    saldoAhorros = CuentaCorriente()
+
+    """-----------------------------
+    Metodos
+    -----------------------------"""
+    
+    def ConsignarValor(self, saldo):
+        nSaldo = self.saldo + ""
+        self.saldo = nSaldo
+        return "Ingrese valor a consignar"
+    
+    def RetirarValor(self, saldo):
+        nSaldo = self.saldo - ""
+        self.saldo = nSaldo
+        return "Ingrese valor a retirar" + ""
+    
+    def TransferirSaldo(self, CuentaCorriente):
+        self.saldo = self.saldo - self.saldo
+        CuentaCorriente += self.saldo 
+        return "El saldo finnal en la cuenta de ahorros es" + CuentaCorriente
+    
+    def DuplicarSaldo(self):
+        #Forrma 1
+        #self.saldo = self.saldo * 2
+        #Forma 2
+        self.saldo *= 2
+        return "El saldo duplicado en su cuenta es: "+ self.saldo
+
     
