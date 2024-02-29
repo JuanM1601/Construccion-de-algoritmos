46971fe0e12912ec9c7fc2ed7a734d4440b60b94
JuanM1601 <160885176+JuanM1601@users.noreply.github.com>
2024-02-28 20:29:25 -0500

    Update SimuladorBancario.py

--- SimuladorBancario/SimuladorBancario.py
+++ SimuladorBancario/SimuladorBancario.py
@@ -1,6 +1,33 @@
+from CuentaCorriente import CuentaCorriente
+from CuentaAhorros import CuentaAhorros
+
+
 class SimuladorBancario:
-    """
-    Atributos"""
+    """-----------------------------
+    Atributos
+   -----------------------------"""
     cedula =0
     nombre =0
     mesActual =0
+
+    """-----------------------------
+    Asociaciones
+    -----------------------------"""
+    saldoCorriente = CuentaCorriente()
+    saldoAhorros = CuentaAhorros()
+
+    """-----------------------------
+    Metodos
+   -----------------------------"""
+    
+    def ConsignarCorriente(self, saldo):
+        # Aqui va el codigo del metodo
+        return self.saldoCorriente.ConsignarValor(saldo)
+    
+    def CalcularSaldo(self, saldoCorriente, saldoAhorros):
+        sTotal = self.saldoCorriente() + self.saldoAhorros
+        return "Su saldo total es de: " + sTotal
+    
+    def RetirarSaldo(self):
+        self.saldo = 0
+        return "El saldo de la cuenta es: "+ self.saldo
