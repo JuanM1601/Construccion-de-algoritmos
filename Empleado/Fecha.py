a58303d9593a61007404d96c9bc3f585275a77f3
JuanM1601 <160885176+JuanM1601@users.noreply.github.com>
2024-02-28 20:28:20 -0500

    Create Fecha.py

---
+++ Empleado/Fecha.py
@@ -0,0 +1,18 @@
+class Fecha:
+
+    """-----------------------------
+    Atributos
+    -----------------------------"""
+    dia = ""
+    mes = ""
+    anio = ""
+
+    def ConsultarDia(self):
+        return self.dia
+    
+    def ConsultarMes(self):
+        return self.mes
+    
+    def ConsultarAnio(self):
+        return self.anio
+    
